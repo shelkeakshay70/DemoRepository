ab90f604d978a0957d99bbea64cb4141ae68a007
shelkeakshay70 <47059709+shelkeakshay70@users.noreply.github.com>
2019-01-26 21:03:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("Simple java program for jenkins");
+		System.out.println("To test Poll SCM of jenkins");
 	}
 }
